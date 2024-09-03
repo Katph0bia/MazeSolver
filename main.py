05512fce50b1011b5aa621eea4df076d99019b7a
Katph0bia <alexandrejadc@gmail.com>
2024-09-03 00:39:03 +0000

    Second lesson trough. Still need to fix tkinter

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800,600)
+    a_line = Line(Point(50,50), Point(400,400))
+    win.draw_line(a_line, "black")
     win.wait_for_close()
 
 
