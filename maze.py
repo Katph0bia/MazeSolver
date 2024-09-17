e436a608cdf4cae2783c07878cd893802e0f83cc
Katph0bia <alexandrejadc@gmail.com>
2024-09-17 05:31:39 +0000

    Maze breaker added!

--- maze.py
+++ maze.py
@@ -49,3 +49,9 @@ class Maze:
             return
         self._win.redraw()
         time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cells(0, 0)
+        self._cells[self._numcols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cells(self._num_cols - 1, self._num_rows - 1)
