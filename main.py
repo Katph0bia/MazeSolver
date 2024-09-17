7d72f30ee43d26d8dfec61d2afe93eed7a1307e0
Katph0bia <alexandrejadc@gmail.com>
2024-09-17 05:06:19 +0000

    Unittests added

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
-from graphics import Window
+import unittest
 from maze import Maze
 
-def main():
-    num_rows = 12
-    num_cols = 16
-    margin = 50
-    screen_x = 800
-    screen_y = 600
-    cell_size_x = (screen_x - 2 *margin) / num_cols
-    cell_size_y = (screen_y - 2 *margin) / num_rows
-    win = Window(screen_x, screen_y)
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+        len(m1._cells),
+        num_cols,
+        )
+        self.asserEqual(
+        len(m1._cells[0]),
+        num_rows,
+        )
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    def test_maze_create_cells_large(self):
+        num_cols = 16
+        num_rows = 12
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            lem(m1._cells),
+            num_cols
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+        
+    def test_maze_create_cells_small(self):
+        num_cols = 6
+        num_rows = 5
+        m1 = Maze(0, 0, num_cols, num_rows, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
 
-    win.wait_for_close()
 
 
-main()
+if __name__ == "__main__":
+    unittest.main()
